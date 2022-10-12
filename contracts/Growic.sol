f2728294f04ced45522b645f348122be4568a758
cjustinobi <cjustinobi@gmail.com>
2022-10-12 16:10:25 +0100

    Complete Struct task

--- contracts/Growic.sol
+++ contracts/Growic.sol
@@ -7,16 +7,17 @@ contract Struct {
 		uint256 age;
 	}
 
-	User[] public users;
+	mapping(address => User) users;
 
 	function setUserDetails(string calldata _name, uint256 _age) public {
 
-		users.push(User({name: _name, age: _age}));
+		users[msg.sender] = User({name: _name, age: _age});
 
 	}
 
-	function getUserDetail(uint _index) public view returns (string memory name, bool age) {
-		User storage user = users[_index];
+	function getUserDetail() public view returns (string memory name, uint256 age) {
+		User storage user = users[msg.sender];
 		return (user.name, user.age);
 	}
+
 }
